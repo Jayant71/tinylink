66fd6733ab008d0a53f7b31ffb3060e19c7338c6
Jayant71 <110401230+Jayant71@users.noreply.github.com>
2025-11-26 16:47:06 +0530

    feat: Initialize and export Prisma client for database access.

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,7 +2,7 @@
 import 'dotenv/config'
 import { Pool } from 'pg'
 import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client/extension'
 
 const connectionString = process.env.DATABASE_URL!
 
